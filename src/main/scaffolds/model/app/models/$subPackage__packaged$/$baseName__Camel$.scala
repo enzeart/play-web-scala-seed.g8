e3f050dd12acbec4edc059a2a48a162ee6c961d1
Enseart Alwyn Simpson <enzeart@gmail.com>
2020-11-14 15:43:23 -0500

    Added scaffold for models

---
+++ src/main/scaffolds/model/app/models/$subPackage__packaged$/$baseName__Camel$.scala
@@ -0,0 +1,10 @@
+package models
+
+import play.api.libs.json._
+
+object $baseName;format="Camel"$ {
+
+  implicit val $baseName;format="camel"$Format: Format[$baseName;format="Camel"$] = Json.format[$baseName;format="Camel"$]
+}
+
+case class $baseName;format="Camel"$(_deleteThisField: String)
