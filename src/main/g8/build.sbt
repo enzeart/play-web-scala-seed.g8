48f8bdecdb6e5c616465a3bf55441653ab121f3d
Enseart Alwyn Simpson <enzeart@gmail.com>
2020-11-20 22:18:30 -0500

    Added template for an app-specific plugin

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -3,7 +3,7 @@ import  $name;format="space,Camel"$Dependencies._
 Global / onChangedBuildSource := ReloadOnSourceChanges
 
 lazy val $name;format="space,camel"$ = (project in file("."))
-  .enablePlugins(PlayScala)
+  .enablePlugins(PlayScala, $name;format="space,Camel"$Plugin)
   .settings(
     name := "$name$",
     libraryDependencies ++= rootDependencies,
---
+++ src/main/g8/project/$name__space,Camel$Plugin.scala
@@ -0,0 +1,42 @@
+import giter8.ScaffoldPlugin
+import giter8.ScaffoldPlugin.autoImport.g8Scaffold
+import sbt.{AutoPlugin, Def, _}
+import complete.DefaultParsers._
+import sbt.internal.util.complete.Parser
+
+object $name;format="space,Camel"$Plugin extends AutoPlugin {
+
+  object autoImport {
+    lazy val $name;format="space,camel"$Controller = inputKey[Unit]("Create a controller from the giter8 scaffold")
+    lazy val $name;format="space,camel"$Model = inputKey[Unit]("Create a model from the giter8 scaffold")
+    lazy val $name;format="space,camel"$Module = inputKey[Unit]("Create a guice module from the giter8 scaffold")
+  }
+
+  import autoImport._
+
+  val baseNameParser: Parser[String] = Space ~> token(ScalaID).examples("<baseName>")
+
+  val $name;format="space,camel"$ControllerTask = Def.inputTaskDyn {
+    g8Scaffold.toTask(s" controller --baseName=\${baseNameParser.parsed}")
+  }
+
+  val $name;format="space,camel"$ModelTask = Def.inputTaskDyn {
+    g8Scaffold.toTask(s" model --baseName=\${baseNameParser.parsed}")
+  }
+
+  val $name;format="space,camel"$ModuleTask = Def.inputTaskDyn {
+    g8Scaffold.toTask(s" module --baseName=\${baseNameParser.parsed}")
+  }
+
+  val base$name;format="space,Camel"$Settings: Seq[Def.Setting[_]] = Seq(
+    $name;format="space,camel"$Controller := $name;format="space,camel"$ControllerTask.evaluated,
+    $name;format="space,camel"$Model := $name;format="space,camel"$ModelTask.evaluated,
+    $name;format="space,camel"$Module := $name;format="space,camel"$ModuleTask.evaluated
+  )
+
+  override val trigger = noTrigger
+
+  override val requires: Plugins = ScaffoldPlugin
+
+  override lazy val projectSettings: Seq[Def.Setting[_]] = base$name;format="space,Camel"$Settings
+}
