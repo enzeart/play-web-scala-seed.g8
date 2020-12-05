fdedb82632d6debdf5ce219649dbf726611fb87c
Enseart Alwyn Simpson <enzeart@gmail.com>
2020-12-05 12:53:58 -0500

    Clean up Auth schema a bit

--- src/main/g8/app/graphql/AuthSchema.scala
+++ src/main/g8/app/graphql/AuthSchema.scala
@@ -1,46 +1,46 @@
 package graphql
 import models.auth.{CommonProfile, UserProfile}
 import sangria.schema._
+import sangria.macros.derive._
 
 object AuthSchema {
+
+  private val IdFieldDescription          = "The user's id"
+  private val UserNameFieldDescription    = "The user's account name"
+  private val EmailFieldDescription       = "The user's email address"
+  private val FirstNameFieldDescription   = "This user's first name"
+  private val FamilyNameFieldDescription  = "The user's family name"
+  private val DisplayNameFieldDescription = "The user's publicly displayed name"
+  private val RolesFieldDescription       = "The user's account roles"
 
   val UserProfile: InterfaceType[Unit, UserProfile] = InterfaceType(
     "UserProfile",
     "The profile of an authenticated user",
     fields[Unit, UserProfile](
-      Field("id", StringType, Some("The user's id"), resolve = _.value.id),
-      Field("userName", OptionType(StringType), Some("The user's account name"), resolve = _.value.userName),
-      Field("email", OptionType(StringType), Some("The user's email address"), resolve = _.value.email),
-      Field("firstName", OptionType(StringType), Some("This user's first name"), resolve = _.value.firstName),
-      Field("familyName", OptionType(StringType), Some("The user's family name"), resolve = _.value.familyName),
-      Field(
-        "displayName",
-        OptionType(StringType),
-        Some("The the name displayed for the user"),
-        resolve = _.value.displayName
-      ),
-      Field("roles", ListType(StringType), Some("The user's account roles"), resolve = _.value.roles.toSeq)
+      Field("id", StringType, Option(IdFieldDescription), resolve = _.value.id),
+      Field("userName", OptionType(StringType), Option(UserNameFieldDescription), resolve = _.value.userName),
+      Field("email", OptionType(StringType), Option(EmailFieldDescription), resolve = _.value.email),
+      Field("firstName", OptionType(StringType), Option(FirstNameFieldDescription), resolve = _.value.firstName),
+      Field("familyName", OptionType(StringType), Option(FamilyNameFieldDescription), resolve = _.value.familyName),
+      Field("displayName", OptionType(StringType), Option(DisplayNameFieldDescription), resolve = _.value.displayName),
+      Field("roles", ListType(StringType), Some(RolesFieldDescription), resolve = _.value.roles.toSeq)
     )
   )
 
-  val CommonProfile: ObjectType[Unit, CommonProfile] = ObjectType(
-    "CommonProfile",
-    "A profile with all of the common fields shared across all profile types",
-    interfaces[Unit, CommonProfile](UserProfile),
-    fields[Unit, CommonProfile](
-      Field("id", StringType, Some("The user's id"), resolve = _.value.id),
-      Field("userName", OptionType(StringType), Some("The user's account name"), resolve = _.value.userName),
-      Field("email", OptionType(StringType), Some("The user's email address"), resolve = _.value.email),
-      Field("firstName", OptionType(StringType), Some("This user's first name"), resolve = _.value.firstName),
-      Field("familyName", OptionType(StringType), Some("The user's family name"), resolve = _.value.familyName),
-      Field(
-        "displayName",
-        OptionType(StringType),
-        Some("The the name displayed for the user"),
-        resolve = _.value.displayName
-      ),
-      Field("roles", ListType(StringType), Some("The user's account roles"), resolve = _.value.roles.toSeq)
-    )
+  val CommonProfile: ObjectType[Unit, CommonProfile] = deriveObjectType[Unit, CommonProfile](
+    ObjectTypeName("CommonProfile"),
+    ObjectTypeDescription("A profile with all of the common fields shared across all profile types"),
+    Interfaces(UserProfile),
+    ReplaceField(
+      "roles",
+      Field("roles", ListType(StringType), description = Option(RolesFieldDescription), resolve = _.value.roles.toSeq)
+    ),
+    DocumentField(fieldName = "id", description = IdFieldDescription),
+    DocumentField(fieldName = "userName", description = UserNameFieldDescription),
+    DocumentField(fieldName = "email", description = EmailFieldDescription),
+    DocumentField(fieldName = "firstName", description = FirstNameFieldDescription),
+    DocumentField(fieldName = "familyName", description = FamilyNameFieldDescription),
+    DocumentField(fieldName = "displayName", description = DisplayNameFieldDescription)
   )
 
   val UserProfilesField: Field[GraphQLContext, Unit] =
