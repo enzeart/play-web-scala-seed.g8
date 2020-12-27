e268095f2c0bb47e0c2b9239bfb5b38490c77142
Enseart Alwyn Simpson <enzeart@gmail.com>
2020-12-27 08:04:58 -0500

    Escape dollar sign

--- src/main/g8/app/graphql/ErrorMessages.scala
+++ src/main/g8/app/graphql/ErrorMessages.scala
@@ -5,5 +5,5 @@ import sangria.ast.OperationType
 object ErrorMessages {
 
   def unsupportedOperationType(operationType: OperationType): String =
-    s"Unsupported operation type: ${operationType.toString.toLowerCase}"
+    s"Unsupported operation type: \${operationType.toString.toLowerCase}"
 }
