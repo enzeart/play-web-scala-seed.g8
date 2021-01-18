8f733aaa9486c8fa255b17e5f54988ffc8968bf7
Enseart Alwyn Simpson <enzeart@gmail.com>
2021-01-18 12:27:16 -0500

    Changed template structure of app-interceptor schematic

--- src/main/g8/angular/app-schematics/src/proxy-config/index.ts
+++ src/main/g8/angular/app-schematics/src/proxy-config/index.ts
@@ -4,12 +4,10 @@ import { buildRelativePath } from "../utility/find-module";
 
 export function proxyConfig(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-
     const workspaceConfig = parseWorkspaceConfig(tree);
     const project = workspaceConfig.defaultProject;
     workspaceConfig.projects[project].architect.serve.options.proxyConfig = buildRelativePath('/', FilePaths.PROXY_CONFIGURATION);
     tree.overwrite(FilePaths.WORKSPACE_CONFIGURATION, JSON.stringify(workspaceConfig, null, 2));
-
     return !tree.exists(FilePaths.PROXY_CONFIGURATION) ? mergeWith(apply(url('./files'), [template({})]))(tree, _context) : tree;
   };
 }
--- src/main/g8/angular/app-schematics/src/utils/files.ts
+++ src/main/g8/angular/app-schematics/src/utils/files.ts
@@ -1,4 +1,5 @@
-import { SchematicsException, Tree } from "@angular-devkit/schematics";
+import { SchematicsException, Tree } from '@angular-devkit/schematics';
+import * as ts from 'typescript';
 
 export enum FilePaths {
   APP_MODULE = '/src/app/app.module.ts',
@@ -9,6 +10,12 @@ export enum FilePaths {
 
 export const parseWorkspaceConfig = (tree: Tree): any => {
   const workspaceConfigBuffer = tree.read(FilePaths.WORKSPACE_CONFIGURATION);
-  if (!workspaceConfigBuffer) throw new SchematicsException("Could not find angular.json");
+  if (!workspaceConfigBuffer) throw new SchematicsException('Could not find angular.json');
   return JSON.parse(workspaceConfigBuffer.toString('utf-8'));
+}
+
+export const createAppModuleSourceFile = (tree: Tree): ts.SourceFile => {
+  const appModuleBuffer = tree.read(FilePaths.APP_MODULE);
+  if (!appModuleBuffer) throw new SchematicsException(`Could not find ${FilePaths.APP_MODULE}`);
+  return ts.createSourceFile(FilePaths.APP_MODULE, appModuleBuffer.toString('utf-8'), ts.ScriptTarget.Latest, true);
 }
