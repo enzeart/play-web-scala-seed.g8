2a4520cbc0cfc4e10de95cd872089356e8ab8412
Enseart Alwyn Simpson <enzeart@gmail.com>
2021-01-18 12:41:42 -0500

    Cleaned up app interceptor file

--- src/main/g8/angular/app-schematics/src/app-interceptor/files/src/app/core/http-interceptors/app.interceptor.ts
+++ src/main/g8/angular/app-schematics/src/app-interceptor/files/src/app/core/http-interceptors/app.interceptor.ts
@@ -9,39 +9,37 @@ import {
 import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 
+const addAjaxRequestResolverHeaders = (
+  request: HttpRequest<unknown>
+): HttpRequest<unknown> => {
+  return request.clone({
+    headers: request.headers.set("X-Requested-With", "XMLHttpRequest"),
+  });
+};
+
+const reloadOnSessionTimeout = (
+  source: Observable<HttpEvent<unknown>>
+): Observable<HttpEvent<unknown>> => {
+  return source.pipe(
+    tap(
+      () => {},
+      (error) => {
+        if (error instanceof HttpErrorResponse && error.status === 401)
+          window.location.reload(true);
+      }
+    )
+  );
+};
+
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
   constructor() {}
-
-  private static addAjaxRequestResolverHeaders(
-    request: HttpRequest<unknown>
-  ): HttpRequest<unknown> {
-    return request.clone({
-      headers: request.headers.set("X-Requested-With", "XMLHttpRequest"),
-    });
-  }
-
-  private static reloadOnSessionTimeout<T>(source: Observable<T>) {
-    return source.pipe(
-      tap(
-        () => {},
-        (error) => {
-          if (error instanceof HttpErrorResponse && error.status === 401)
-            window.location.reload(true);
-        }
-      )
-    );
-  }
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    const modifiedRequest = AppInterceptor.addAjaxRequestResolverHeaders(
-      request
-    );
-    return next
-      .handle(modifiedRequest)
-      .pipe(AppInterceptor.reloadOnSessionTimeout);
+    const modifiedRequest = addAjaxRequestResolverHeaders(request);
+    return next.handle(modifiedRequest).pipe(reloadOnSessionTimeout);
   }
 }
