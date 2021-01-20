ee840be99a93294d1984fe4b7896c489965aeca1
Enseart Alwyn Simpson <enzeart@gmail.com>
2021-01-20 11:24:40 -0500

    Added redirect logic to SpaRootComponent template

--- src/main/g8/angular/app-schematics/src/spa-root/files/src/app/core/components/spa-root/spa-root.component.ts
+++ src/main/g8/angular/app-schematics/src/spa-root/files/src/app/core/components/spa-root/spa-root.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+const redirectRouteQueryParam = 'spa-redirect-route';
 
 @Component({
   selector: 'app-spa-root',
@@ -6,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spa-root.component.css'],
 })
 export class SpaRootComponent implements OnInit {
-  constructor() {}
+  readonly originalUrl: string;
 
-  ngOnInit(): void {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    this.originalUrl = router.url;
+  }
+
+  ngOnInit(): void {
+    this.followRedirectRoute();
+  }
+
+  followRedirectRoute(): void {
+    const { queryParams } = this.activatedRoute.snapshot;
+    const redirectRoute = queryParams[redirectRouteQueryParam];
+    if (redirectRoute !== this.originalUrl) {
+      this.router.navigate(redirectRoute);
+    }
+  }
 }
