8651e9b3454a6855edd104c4d4253ebd750c6be2
Enseart Alwyn Simpson <enzeart@gmail.com>
2021-01-26 18:33:00 -0500

    Removed session timeout logic from AppInterceptor

--- src/main/g8/angular/init-schematics/src/app-interceptor/files/src/app/core/http-interceptors/app.interceptor.ts
+++ src/main/g8/angular/init-schematics/src/app-interceptor/files/src/app/core/http-interceptors/app.interceptor.ts
@@ -1,23 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 const addAjaxRequestResolverHeaders = (request: HttpRequest<unknown>): HttpRequest<unknown> => {
   return request.clone({
     headers: request.headers.set('X-Requested-With', 'XMLHttpRequest'),
   });
-};
-
-const reloadOnSessionTimeout = (source: Observable<HttpEvent<unknown>>): Observable<HttpEvent<unknown>> => {
-  return source.pipe(
-    tap(
-      () => {},
-      (error) => {
-        if (error instanceof HttpErrorResponse && error.status === 401) window.location.reload(true);
-      }
-    )
-  );
 };
 
 @Injectable()
@@ -26,6 +14,6 @@ export class AppInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const modifiedRequest = addAjaxRequestResolverHeaders(request);
-    return next.handle(modifiedRequest).pipe(reloadOnSessionTimeout);
+    return next.handle(modifiedRequest);
   }
 }
