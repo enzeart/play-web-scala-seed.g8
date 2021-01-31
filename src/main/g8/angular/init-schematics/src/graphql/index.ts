78805f7e52076573996ae9976db76ff5934e2dc9
Enseart Alwyn Simpson <enzeart@gmail.com>
2021-01-31 17:25:09 -0500

    Prettier

--- src/main/g8/angular/init-schematics/src/graphql/index.ts
+++ src/main/g8/angular/init-schematics/src/graphql/index.ts
@@ -5,11 +5,11 @@ import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 export function graphql(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const templateSources = applyStandardTemplates();
-    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/cli' }))
-    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/typescript' }))
-    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/typescript-apollo-angular' }))
-    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/typescript-operations' }))
-    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/introspection' }))
+    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/cli' }));
+    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/typescript' }));
+    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/typescript-apollo-angular' }));
+    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/typescript-operations' }));
+    _context.addTask(new NodePackageInstallTask({ packageName: '@graphql-codegen/introspection' }));
     return mergeWith(templateSources)(tree, _context);
   };
 }
