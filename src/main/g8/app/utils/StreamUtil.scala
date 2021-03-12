8a11195359c6170becafa2ddd1c34c245c80e6a2
Enseart Alwyn Simpson <enzeart@gmail.com>
2021-03-11 21:16:16 -0500

    Simplifying StreamUtils

--- src/main/g8/app/utils/StreamUtil.scala
+++ src/main/g8/app/utils/StreamUtil.scala
@@ -1,36 +1,34 @@
 package utils
 
 import akka.actor.typed.ActorRef
-import akka.stream.{Materializer, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import akka.stream.typed.scaladsl.{ActorSink, ActorSource}
+import akka.stream.{Materializer, OverflowStrategy}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
-object StreamUtil {
+object StreamUtils {
 
   def actorFlow[In, B, Out](
       inputTransform: In => B,
-      inputRef: ActorRef[Out] => Future[ActorRef[B]],
+      inputRef: ActorRef[Out] => ActorRef[B],
       inputOnCompleteMessage: B,
       inputOnFailureMessage: Throwable => B,
       outputCompletionMatcher: PartialFunction[Out, Unit] = PartialFunction.empty,
       outputFailureMatcher: PartialFunction[Out, Throwable] = PartialFunction.empty,
       outputBufferSize: Int = 16,
       outputOverflowStrategy: OverflowStrategy = OverflowStrategy.dropNew
-  )(implicit mat: Materializer, ec: ExecutionContext): Future[Flow[In, Out, _]] = {
+  )(implicit mat: Materializer, ec: ExecutionContext): Flow[In, Out, _] = {
     val (outputChannel, publisher) = ActorSource
       .actorRef[Out](outputCompletionMatcher, outputFailureMatcher, outputBufferSize, outputOverflowStrategy)
       .toMat(Sink.asPublisher(false))(Keep.both)
       .run()
 
-    for (ref <- inputRef(outputChannel)) yield {
-      Flow.fromSinkAndSourceCoupled(
-        Flow
-          .fromFunction[In, B](inputTransform)
-          .to(ActorSink.actorRef[B](ref, inputOnCompleteMessage, inputOnFailureMessage)),
-        Source.fromPublisher(publisher)
-      )
-    }
+    Flow.fromSinkAndSourceCoupled(
+      Flow
+        .fromFunction[In, B](inputTransform)
+        .to(ActorSink.actorRef[B](inputRef(outputChannel), inputOnCompleteMessage, inputOnFailureMessage)),
+      Source.fromPublisher(publisher)
+    )
   }
 }
