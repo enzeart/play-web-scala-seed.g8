fa71b791f79833a6c697f199779c5872447bbc2a
Enseart Alwyn Simpson <enzeart@gmail.com>
2021-11-21 20:39:55 -0500

    Removed ConfigModule and moved config logic to AppModule

--- src/main/g8/app/modules/AppModule.scala
+++ src/main/g8/app/modules/AppModule.scala
@@ -1,13 +1,24 @@
 package modules
 
-import com.google.inject.AbstractModule
 import com.google.inject.assistedinject.FactoryModuleBuilder
 import graphql.GraphQLContextFactory
+
+import com.google.inject.{AbstractModule, Provides}
+import config.AppConfig
 import net.codingwell.scalaguice.ScalaModule
+import play.api.Configuration
+import pureconfig.ConfigSource
+import pureconfig.generic.auto._
+
+import javax.inject.Singleton
 
 class AppModule extends AbstractModule with ScalaModule {
 
   override def configure(): Unit = {
     install((new FactoryModuleBuilder).build(classOf[GraphQLContextFactory]))
   }
+
+  @Provides @Singleton
+  def provideAppConfig(configuration: Configuration): AppConfig =
+    ConfigSource.fromConfig(configuration.underlying).at("app").loadOrThrow[AppConfig]
 }
