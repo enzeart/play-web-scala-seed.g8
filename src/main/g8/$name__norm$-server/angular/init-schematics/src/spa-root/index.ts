928364fe25ab81dce5a9fb8b500f8554607e61b7
Enseart Alwyn Simpson <enzeart@gmail.com>
2022-02-26 15:00:25 -0500

    More refactors

--- src/main/g8/$name__norm$-server/angular/init-schematics/src/spa-root/index.ts
+++ src/main/g8/$name__norm$-server/angular/init-schematics/src/spa-root/index.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular-devkit/schematics';
 import {
   applyTemplates,
-  createAppRoutingModuleSourceFile,
+  // createAppRoutingModuleSourceFile,
   createSourceFile,
   FilePaths,
 } from '../utils/files';
@@ -20,14 +20,14 @@ import { buildRelativePath } from '@schematics/angular/utility/find-module';
 import * as ts from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
 import { InsertChange } from '@schematics/angular/utility/change';
 
-const routesVariableStatementText = 'const routes: Routes = [];';
+// const routesVariableStatementText = 'const routes: Routes = [];';
 
 const spaRootComponentClassifiedName = 'SpaRootComponent';
 
-const spaRootRouteDefinitionText = `
-  { path: '', component: ${spaRootComponentClassifiedName}, pathMatch: 'full' },
-  { path: '**', redirectTo: '/' }
-`;
+// const spaRootRouteDefinitionText = `
+//   { path: '', component: ${spaRootComponentClassifiedName}, pathMatch: 'full' },
+//   { path: '**', redirectTo: '/' }
+// `;
 
 const spaRootComponentRelativePath = (from: string) =>
   buildRelativePath(
@@ -35,26 +35,72 @@ const spaRootComponentRelativePath = (from: string) =>
     '/src/app/core/components/spa-root/spa-root.component'
   );
 
-const findRoutesArrayLiteral = (
-  sourceFile: ts.SourceFile
-): ts.ArrayLiteralExpression => {
-  const routesArrayVariableStatement = findNodes(
+// const findRoutesArrayLiteral = (
+//   sourceFile: ts.SourceFile
+// ): ts.ArrayLiteralExpression => {
+//   const routesArrayVariableStatement = findNodes(
+//     sourceFile,
+//     ts.isVariableStatement
+//   ).find((s) => s.getText() === routesVariableStatementText);
+//
+//   if (!routesArrayVariableStatement)
+//     throw new SchematicsException('Could not find routes variable statement');
+//
+//   const routesArrayLiteralExpression = findNodes(
+//     routesArrayVariableStatement,
+//     ts.isArrayLiteralExpression
+//   )[0];
+//
+//   if (!routesArrayLiteralExpression)
+//     throw new SchematicsException('Could not find routes array literal');
+//
+//   return routesArrayLiteralExpression;
+// };
+
+const editAppRoutingModule = (tree: Tree): void => {
+  const sourceFile = createSourceFile(FilePaths.APP_ROUTING_MODULE, tree);
+  const routesVariableStatement = findNodes(
     sourceFile,
     ts.isVariableStatement
-  ).find((s) => s.getText() === routesVariableStatementText);
+  ).find((statement) => statement.getText() === 'const routes: Routes = [];');
 
-  if (!routesArrayVariableStatement)
-    throw new SchematicsException('Could not find routes variable statement');
+  if (!routesVariableStatement)
+    throw new SchematicsException('Could not find "routes" variable statement');
 
-  const routesArrayLiteralExpression = findNodes(
-    routesArrayVariableStatement,
+  const routesExpression = findNodes(
+    routesVariableStatement,
     ts.isArrayLiteralExpression
   )[0];
 
-  if (!routesArrayLiteralExpression)
-    throw new SchematicsException('Could not find routes array literal');
+  if (!routesExpression)
+    throw new SchematicsException('Could not find "routes" array literal');
 
-  return routesArrayLiteralExpression;
+  const changes = [
+    insertImport(
+      sourceFile,
+      FilePaths.APP_ROUTING_MODULE,
+      spaRootComponentClassifiedName,
+      spaRootComponentRelativePath(FilePaths.APP_ROUTING_MODULE)
+    ),
+    new InsertChange(
+      FilePaths.APP_ROUTING_MODULE,
+      routesExpression.end - 1,
+      `
+        { path: '', component: ${spaRootComponentClassifiedName}, pathMatch: 'full' },
+        { path: '**', redirectTo: '/' }
+      `
+    ),
+  ];
+
+  const recorder = tree.beginUpdate(FilePaths.APP_ROUTING_MODULE);
+
+  for (const change of changes) {
+    if (change instanceof InsertChange) {
+      recorder.insertLeft(change.pos, change.toAdd);
+    }
+  }
+
+  tree.commitUpdate(recorder);
 };
 
 const editAppModule = (tree: Tree): void => {
@@ -107,40 +153,40 @@ const editEnvironmentConfiguration = (path: string, tree: Tree): void => {
 export function spaRoot(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const templateSources = applyTemplates();
-    const appRoutingModuleSourceFile = createAppRoutingModuleSourceFile(tree);
-    const importSpaRootComponent = insertImport(
-      appRoutingModuleSourceFile,
-      FilePaths.APP_ROUTING_MODULE,
-      spaRootComponentClassifiedName,
-      spaRootComponentRelativePath(FilePaths.APP_ROUTING_MODULE)
-    );
+    // const appRoutingModuleSourceFile = createAppRoutingModuleSourceFile(tree);
+    // const importSpaRootComponent = insertImport(
+    //   appRoutingModuleSourceFile,
+    //   FilePaths.APP_ROUTING_MODULE,
+    //   spaRootComponentClassifiedName,
+    //   spaRootComponentRelativePath(FilePaths.APP_ROUTING_MODULE)
+    // );
+    //
+    // const routesArrayLiteral = findRoutesArrayLiteral(
+    //   appRoutingModuleSourceFile
+    // );
+    // const routesArrayPosition = routesArrayLiteral.end - 1;
+    // const appRoutingModuleChanges = [
+    //   new InsertChange(
+    //     FilePaths.APP_ROUTING_MODULE,
+    //     routesArrayPosition,
+    //     spaRootRouteDefinitionText
+    //   ),
+    //   importSpaRootComponent,
+    // ];
+    //
+    // const appRoutingModuleRecorder = tree.beginUpdate(
+    //   FilePaths.APP_ROUTING_MODULE
+    // );
+    //
+    // for (const change of appRoutingModuleChanges) {
+    //   if (change instanceof InsertChange) {
+    //     appRoutingModuleRecorder.insertLeft(change.pos, change.toAdd);
+    //   }
+    // }
+    //
+    // tree.commitUpdate(appRoutingModuleRecorder);
 
-    const routesArrayLiteral = findRoutesArrayLiteral(
-      appRoutingModuleSourceFile
-    );
-    const routesArrayPosition = routesArrayLiteral.end - 1;
-    const appRoutingModuleChanges = [
-      new InsertChange(
-        FilePaths.APP_ROUTING_MODULE,
-        routesArrayPosition,
-        spaRootRouteDefinitionText
-      ),
-      importSpaRootComponent,
-    ];
-
-    const appRoutingModuleRecorder = tree.beginUpdate(
-      FilePaths.APP_ROUTING_MODULE
-    );
-
-    for (const change of appRoutingModuleChanges) {
-      if (change instanceof InsertChange) {
-        appRoutingModuleRecorder.insertLeft(change.pos, change.toAdd);
-      }
-    }
-
-    tree.commitUpdate(appRoutingModuleRecorder);
-
-
+    editAppRoutingModule(tree);
     editAppModule(tree);
     editEnvironmentConfiguration(FilePaths.ENVIRONMENT, tree);
     editEnvironmentConfiguration(FilePaths.ENVIRONMENT_PROD, tree);
