65f4f0b78c1ca675fcb0eb347a49df2a092b3267
Enseart Alwyn Simpson <enzeart@gmail.com>
2023-07-17 13:34:46 -0400

    Move app-component logic to ng-add schematic

--- src/main/g8/$name__norm$-server/angular/init-schematics/src/ng-add/index.ts
+++ src/main/g8/$name__norm$-server/angular/init-schematics/src/ng-add/index.ts
@@ -1,20 +1,30 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import {RunSchematicTask} from '@angular-devkit/schematics/tasks';
+import {
+  apply,
+  MergeStrategy,
+  mergeWith,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
+import { RunSchematicTask } from '@angular-devkit/schematics/tasks';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function ngAdd(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const environmentsTask = _context.addTask(new RunSchematicTask("@schematics/angular", "environments", {}));
+    const templateSource = apply(url('./files'), [template({})])
+    const environmentsTaskId = _context.addTask(new RunSchematicTask("@schematics/angular", "environments", {}));
     _context.addTask(new RunSchematicTask("core", { project: _options.project}));
     _context.addTask(new RunSchematicTask("proxy-config", { project: _options.project}));
-    _context.addTask(new RunSchematicTask("app-component", {}));
-    _context.addTask(new RunSchematicTask("spa-root", {}), [environmentsTask]);
+    // _context.addTask(new RunSchematicTask("app-component", {}));
+    _context.addTask(new RunSchematicTask("spa-root", {}), [environmentsTaskId]);
     _context.addTask(new RunSchematicTask("app-interceptor", {}));
     _context.addTask(new RunSchematicTask("graphql", {}));
     _context.addTask(new RunSchematicTask("shared-module", {}));
     // _context.addTask(new RunSchematicTask("apollo-angular", "ng-add", {endpoint: "/api/graphql"}));
-    return tree;
+    return mergeWith(templateSource, MergeStrategy.Overwrite)(tree, _context);
   };
 }
