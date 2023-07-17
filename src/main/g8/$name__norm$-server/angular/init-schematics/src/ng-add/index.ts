fef7fb13a94cc532e8b8ad539daacd246df9960d
Enseart Alwyn Simpson <enzeart@gmail.com>
2023-07-17 16:57:48 -0400

    Incorporated app-interceptor schematic into ng-add schematic

--- src/main/g8/$name__norm$-server/angular/init-schematics/src/ng-add/index.ts
+++ src/main/g8/$name__norm$-server/angular/init-schematics/src/ng-add/index.ts
@@ -3,14 +3,23 @@ import {
   MergeStrategy,
   mergeWith,
   Rule,
-  SchematicContext,
+  SchematicContext, SchematicsException,
   template,
   Tree,
   url
 } from '@angular-devkit/schematics';
 import { RunSchematicTask } from '@angular-devkit/schematics/tasks';
 import { JSONFile } from '@schematics/angular/utility/json-file';
+import { addProviderToModule } from '@schematics/angular/utility/ast-utils';
+import * as ts from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
+import { buildRelativePath } from '@schematics/angular/utility/find-module';
+import { InsertChange } from '@schematics/angular/utility/change';
 
+const appModulePath = '/src/app/app.module.ts';
+const httpInterceptorsDirectoryPath = '/src/app/core/http-interceptors/';
+const httpInterceptorProviders = 'httpInterceptorProviders';
+const cookieService = 'CookieService';
+const ngxCookieService = 'ngx-cookie-service';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -18,28 +27,48 @@ export function ngAdd(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const templateSource = apply(url('./files'), [template({})]);
     const packageJsonFile = new JSONFile(tree, '/package.json');
-    const workspaceConfigurationFile = new JSONFile(tree, '/angular.json')
+    const workspaceConfigurationFile = new JSONFile(tree, '/angular.json');
 
     packageJsonFile.modify(['scripts', 'build'], 'ng build --outputPath=../public');
     packageJsonFile.modify(['scripts', 'prettier'], 'npx prettier --write "src/**/*.ts"');
-    packageJsonFile.modify(['scripts', 'gqlcodegen'], 'graphql-codegen && npm run prettier');
 
     workspaceConfigurationFile.modify(['projects', _options.project, 'schematics', '@schematics/angular:component'], {
       displayBlock: true,
     });
+
+    packageJsonFile.modify(['scripts', 'gqlcodegen'], 'graphql-codegen && npm run prettier');
+
     workspaceConfigurationFile.modify(['projects', _options.project, 'architect', 'serve', 'options'], {
       proxyConfig: './proxy.conf.js',
     });
+
+    addProviderToAppModule(tree, httpInterceptorProviders, buildRelativePath(appModulePath, httpInterceptorsDirectoryPath));
+    addProviderToAppModule(tree, cookieService, ngxCookieService);
 
     const environmentsTaskId = _context.addTask(new RunSchematicTask("@schematics/angular", "environments", {}));
     // _context.addTask(new RunSchematicTask("core", { project: _options.project}));
     // _context.addTask(new RunSchematicTask("proxy-config", { project: _options.project}));
     // _context.addTask(new RunSchematicTask("app-component", {}));
     _context.addTask(new RunSchematicTask("spa-root", {}), [environmentsTaskId]);
-    _context.addTask(new RunSchematicTask("app-interceptor", {}));
+    // _context.addTask(new RunSchematicTask("app-interceptor", {}));
     // _context.addTask(new RunSchematicTask("graphql", {}));
     _context.addTask(new RunSchematicTask("shared-module", {}));
     // _context.addTask(new RunSchematicTask("apollo-angular", "ng-add", {endpoint: "/api/graphql"}));
     return mergeWith(templateSource, MergeStrategy.Overwrite)(tree, _context);
   };
+}
+
+function createSourceFile(tree: Tree, path: string): ts.SourceFile {
+  const buffer = tree.read(path);
+  if (!buffer) throw new SchematicsException(`Failed to read file at path ${path}`);
+  return ts.createSourceFile(path, buffer.toString(), ts.ScriptTarget.Latest, true);
+}
+
+function addProviderToAppModule(tree: Tree, classifiedName: string, importPath: string): void {
+  const changes = addProviderToModule(createSourceFile(tree, appModulePath), appModulePath, classifiedName, importPath);
+  const recorder = tree.beginUpdate(appModulePath);
+  for (const change of changes) {
+    if (change instanceof InsertChange) recorder.insertLeft(change.pos, change.toAdd);
+  }
+  tree.commitUpdate(recorder);
 }
